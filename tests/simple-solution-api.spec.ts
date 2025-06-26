46e0231c20a3e737629389c2913d77964b51eed2
Jekaterina <mihhailova.jekaterina@gmail.com>
2025-06-26 14:51:28 +0300

    Fixed some issues

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 import { OrderDto } from './dto/order-dto'
 
-test('get order with correct id should receive code 200', async ({ request }) => {
+test('Get order details with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
   // Log the response status, body and headers
@@ -13,7 +13,9 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   expect(response.status()).toBe(200)
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
+test('Change order by post method with correct data should receive code 201', async ({
+  request,
+}) => {
   const requestBody = OrderDto.createOrderWithRandomData()
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: requestBody,
@@ -22,34 +24,41 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('request body:', requestBody)
   expect.soft(response.status()).toBe(StatusCodes.OK)
 })
-test('get order with incorrect id 0 should receive code 400 Bad request', async ({ request }) => {
+test('Get order details with incorrect id 0 should receive code 400 Bad request', async ({
+  request,
+}) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
-test('get order with incorrect id 11 should receive code 400 Bad request', async ({ request }) => {
+test('Get order details with incorrect id 11 should receive code 400 Bad request', async ({
+  request,
+}) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
-test('get order with incorrect id test should receive code 400 Bad request', async ({
+test('Get order details with incorrect id test should receive code 400 Bad request', async ({
   request,
 }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/test')
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
-test('post order with incorrect data should receive code 415', async ({ request }) => {
+test('Change order by post method with incorrect data should receive code 415', async ({
+  request,
+}) => {
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: 'test',
   })
-  // Log the response status and body
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
 })
 
-test('post order with correct data should receive code 200', async ({ request }) => {
+test('Change order by post method with correct data should receive code 200', async ({
+  request,
+}) => {
   // prepare request body
   const requestBody = {
     status: 'OPEN',
@@ -67,52 +76,4 @@ test('post order with correct data should receive code 200', async ({ request })
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
-})
-
-test('Get order with orderId 0 should receive code 400', async ({ request }) => {
-  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
-
-  const responseBody = await response.json()
-
-  console.log(await response.json())
-
-  expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST)
-
-  console.log('check message')
-
-  expect.soft(responseBody.message).toBe('getById.id: must be greater than or equal to 1')
-
-  test('get order with incorrect id 0 should receive code 400 Bad request', async ({ request }) => {
-    const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
-    expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
-  })
-
-  test('get order with incorrect id 11 should receive code 400 Bad request', async ({
-    request,
-  }) => {
-    const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
-    expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
-  })
-
-  test('get order with incorrect id null should receive code 500', async ({ request }) => {
-    const response = await request.get('https://backend.tallinn-learning.ee/test-orders/')
-    expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
-  })
-
-  test('get order with incorrect id test should receive code 400 Bad request', async ({
-    request,
-  }) => {
-    const response = await request.get('https://backend.tallinn-learning.ee/test-orders/test')
-    expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
-  })
-
-  test('post order with incorrect data should receive code 415', async ({ request }) => {
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-      data: 'test',
-    })
-    // Log the response status and body
-    console.log('response status:', response.status())
-    console.log('response body:', await response.json())
-    expect(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
-  })
 })
